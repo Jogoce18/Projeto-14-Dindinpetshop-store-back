4a4c5d9997ff753bfd952d5c332530f1b6b77c7e
williameiji <william.fujimori@gmail.com>
2022-07-06 17:49:49 -0300

    fix: structure

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import { signIn,signUp } from "../controllers/authController.js";
-import {signinValidator} from "../infra/validators/signinValidator.js";
-import {signupValidator}  from "../infra/validators/validateSignUp.js";
+import { signIn, signUp } from "../controllers/authController.js";
+import signinValidator from "../infra/validators/signinValidator.js";
+import signupValidator from "../infra/validators/validateSignUp.js";
 import loginToken from "../infra/middlewares/loginToken.js";
-import addNewUser from "../infra/middlewares/NewUser";
+import addNewUser from "../infra/middlewares/NewUser.js";
 
 const authRouter = Router();
 
 authRouter.post("/login", signinValidator, loginToken, signIn);
-authRouter.post("/signup",signupValidator,addNewUser,signUp);
+authRouter.post("/signup", signupValidator, addNewUser, signUp);
 
 export default authRouter;
