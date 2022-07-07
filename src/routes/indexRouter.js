7f146e415d93ee7798f162add23cf9615fd2c450
williameiji <william.fujimori@gmail.com>
2022-07-07 15:25:55 -0300

    feat: route to search item

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./authRouter.js";
 import addProductsRouter from "./addProductsRouter.js";
+import searchRouter from "./searchRouter.js";
 
 const router = Router();
 
 router.use(authRouter);
 router.use(addProductsRouter);
+router.use(searchRouter);
 
 export default router;
