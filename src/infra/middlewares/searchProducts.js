b8687a611f902928b15fa3242655c22c186cf42a
williameiji <william.fujimori@gmail.com>
2022-07-07 16:46:45 -0300

    fix: search route

--- src/infra/middlewares/searchProducts.js
+++ src/infra/middlewares/searchProducts.js
@@ -3,20 +3,13 @@ import db from "../../databases/mongo.js";
 async function searchProducts(req, res, next) {
 	const { searchItem } = req.body;
 
-	// const teste = db
-	// 	.collection("products")
-	// 	.createIndex({ name: "text", category: "text" });
-	// console.log(teste);
-
-	console.log(searchItem);
 	try {
 		const products = await db
 			.collection("products")
 			.find({ name: new RegExp(searchItem, "i") })
 			.toArray();
 
-		console.log(products, "a");
-		if (!products) {
+		if (products.length === 0) {
 			return res.sendStatus(404);
 		}
 
